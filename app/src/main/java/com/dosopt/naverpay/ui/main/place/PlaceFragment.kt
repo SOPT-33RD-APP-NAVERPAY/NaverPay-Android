e4d984376d7ec3da2fa25211c3e2c3b2464a935a
librarywon <librarywon@naver.com>
2023-11-21 02:51:31 +0900

    [FEAT] : default Fragment setting

--- app/src/main/java/com/dosopt/naverpay/ui/main/place/PlaceFragment.kt
+++ app/src/main/java/com/dosopt/naverpay/ui/main/place/PlaceFragment.kt
@@ -1,4 +1,26 @@
 package com.dosopt.naverpay.ui.main.place
 
-class PlaceFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.dosopt.naverpay.databinding.FragmentPlaceBinding
+
+class PlaceFragment : Fragment() {
+    private var _binding: FragmentPlaceBinding? = null
+    private val binding: FragmentPlaceBinding
+        get() = requireNotNull(_binding) { "_binding is  null" }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPlaceBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
--- app/src/main/java/com/dosopt/naverpay/ui/main/point/PointFragment.kt
+++ app/src/main/java/com/dosopt/naverpay/ui/main/point/PointFragment.kt
@@ -1,4 +1,26 @@
 package com.dosopt.naverpay.ui.main.point
 
-class PointFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.dosopt.naverpay.databinding.FragmentPointBinding
+
+class PointFragment : Fragment() {
+    private var _binding: FragmentPointBinding? = null
+    private val binding: FragmentPointBinding
+        get() = requireNotNull(_binding) { "_binding is  null" }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPointBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
