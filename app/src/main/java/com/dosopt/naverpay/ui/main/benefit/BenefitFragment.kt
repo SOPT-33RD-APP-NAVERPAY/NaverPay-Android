b20c4218c3c495c6a3d974d7228a67eec2851130
librarywon <librarywon@naver.com>
2023-11-21 02:51:31 +0900

    [FEAT] : default Fragment setting

--- app/src/main/java/com/dosopt/naverpay/ui/main/benefit/BenefitFragment.kt
+++ app/src/main/java/com/dosopt/naverpay/ui/main/benefit/BenefitFragment.kt
@@ -1,4 +1,26 @@
 package com.dosopt.naverpay.ui.main.benefit
 
-class BenefitFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.dosopt.naverpay.databinding.FragmentBenefitBinding
+
+class BenefitFragment : Fragment() {
+    private var _binding: FragmentBenefitBinding? = null
+    private val binding: FragmentBenefitBinding
+    get() = requireNotNull(_binding) { "_binding is  null" }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentBenefitBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
