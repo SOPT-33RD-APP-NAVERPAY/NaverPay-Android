6741b11938647eb51cd274d3cf250be8c3c105be
Junseo511 <prime54@g.skku.edu>
2023-11-23 01:13:06 +0900

    [ADD] : add header and tap button

--- app/src/main/java/com/dosopt/naverpay/ui/main/home/HomeFragment.kt
+++ app/src/main/java/com/dosopt/naverpay/ui/main/home/HomeFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.dosopt.naverpay.R
 import com.dosopt.naverpay.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -14,7 +17,7 @@ class HomeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
@@ -22,5 +25,40 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.tvTabMembership.setOnClickListener {
+            selectTab(binding.tvTabMembership, binding.vTabMembershipBottom)
+            deselectTab(binding.tvTabPay, binding.vTapPayBottom)
+        }
+
+        binding.tvTabPay.setOnClickListener {
+            selectTab(binding.tvTabPay, binding.vTapPayBottom)
+            deselectTab(binding.tvTabMembership, binding.vTabMembershipBottom)
+        }
+    }
+
+    private fun selectTab(textView: TextView, bottomView: View) {
+        textView.setTextColor(ContextCompat.getColor(requireContext(), R.color.bg_white))
+        bottomView.setBackgroundColor(
+            ContextCompat.getColor(
+                requireContext(),
+                R.color.main_lightgreen
+            )
+        )
+    }
+
+    private fun deselectTab(textView: TextView, bottomView: View) {
+        textView.setTextColor(ContextCompat.getColor(requireContext(), R.color.grayscale_gray7))
+        bottomView.setBackgroundColor(
+            ContextCompat.getColor(
+                requireContext(),
+                android.R.color.transparent
+            )
+        )
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
