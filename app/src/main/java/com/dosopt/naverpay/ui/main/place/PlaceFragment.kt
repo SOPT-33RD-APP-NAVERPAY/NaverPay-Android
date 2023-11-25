4b4a1a2f4fa2daea25e3496f19c0d728ed8c29d3
heominhoe <hhmmm1005@naver.com>
2023-11-26 02:33:34 +0900

    [FEAT] : feat itemdecoration, switch tabs and setup adapters

--- app/src/main/java/com/dosopt/naverpay/ui/main/place/PlaceFragment.kt
+++ app/src/main/java/com/dosopt/naverpay/ui/main/place/PlaceFragment.kt
@@ -1,16 +1,23 @@
 package com.dosopt.naverpay.ui.main.place
 
+import android.graphics.Rect
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.RecyclerView
+import com.dosopt.naverpay.R
 import com.dosopt.naverpay.databinding.FragmentPlaceBinding
 
 class PlaceFragment : Fragment() {
     private var _binding: FragmentPlaceBinding? = null
     private val binding: FragmentPlaceBinding
         get() = requireNotNull(_binding) { "_binding is  null" }
+    private val viewModel = PlaceViewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,5 +29,108 @@ class PlaceFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setDiv()
+        setupAdapter()
+        setupTabs()
+    }
+
+    //어댑터 붙이기
+    private fun setupAdapter() {
+        val placeNearbyAdapter = PlaceNearbyAdapter()
+        val placeRecommendAdapter = PlaceRecommendAdapter()
+        val placePaymentAdapter = PlacePaymentAdapter()
+
+        with(binding) {
+            rvPlace.adapter = placeNearbyAdapter
+            rvRecommend.adapter = placeRecommendAdapter
+            rvPlacePayment.adapter = placePaymentAdapter
+        }
+
+        placeNearbyAdapter.submitList(viewModel.mockNearbyplaceList.value.orEmpty())
+        placeRecommendAdapter.submitList(viewModel.mockBrandList.value.orEmpty())
+        placePaymentAdapter.submitList(viewModel.mockOnsitepaymentList.value.orEmpty())
+    }
+
+    //탭 선택시
+    private fun selectTab(textView: TextView, bottomView: View) {
+        textView.setTextColor(ContextCompat.getColor(requireContext(), R.color.bg_black))
+        bottomView.setBackgroundColor(
+            ContextCompat.getColor(
+                requireContext(),
+                R.color.main_lightgreen
+            )
+        )
+    }
+
+    //탭 미선택시
+    private fun deselectTab(textView: TextView, bottomView: View) {
+        textView.setTextColor(ContextCompat.getColor(requireContext(), R.color.grayscale_gray7))
+        bottomView.setBackgroundColor(
+            ContextCompat.getColor(
+                requireContext(),
+                android.R.color.transparent
+            )
+        )
+    }
+
+    //탭 눌렀을 때
+    private fun setupTabs() {
+        with(binding) {
+            tvDome.setOnClickListener {
+                selectTab(tvDome, viRecDome)
+                deselectTab(tvInter, viRecInter)
+            }
+
+            tvInter.setOnClickListener {
+                selectTab(tvInter, viRecInter)
+                deselectTab(tvDome, viRecDome)
+            }
+        }
+    }
+
+    private fun setDiv() {
+        with(binding) {
+            rvPlace.addItemDecoration(VerticalItemDecorator(12))
+            rvPlace.addItemDecoration(HorizontalItemDecorator(8))
+            rvRecommend.addItemDecoration(VerticalItemDecorator(12))
+            rvRecommend.addItemDecoration(HorizontalItemDecorator(6))
+            rvPlacePayment.addItemDecoration(VerticalItemDecorator(12))
+            rvPlacePayment.addItemDecoration(HorizontalItemDecorator(8))
+        }
+    }
+
+    class VerticalItemDecorator(private val divHeight: Int) : RecyclerView.ItemDecoration() {
+        @Override
+        override fun getItemOffsets(
+            outRect: Rect,
+            view: View,
+            parent: RecyclerView,
+            state: RecyclerView.State
+        ) {
+            super.getItemOffsets(outRect, view, parent, state)
+            outRect.top = divHeight
+            outRect.bottom = divHeight
+        }
+    }
+
+    class HorizontalItemDecorator(private val divHeight: Int) : RecyclerView.ItemDecoration() {
+
+        @Override
+        override fun getItemOffsets(
+            outRect: Rect,
+            view: View,
+            parent: RecyclerView,
+            state: RecyclerView.State
+        ) {
+            super.getItemOffsets(outRect, view, parent, state)
+            outRect.left = divHeight
+            outRect.right = divHeight
+        }
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
     }
 }
