e66278b5ae8536bdd5f3e2173252cccda5807359
Junseo511 <prime54@naver.com>
2023-11-27 01:57:04 +0900

    [ADD] : add comments for the readability

--- app/src/main/java/com/dosopt/naverpay/network/service/NaverPayService.kt
+++ app/src/main/java/com/dosopt/naverpay/network/service/NaverPayService.kt
@@ -14,34 +14,41 @@ import retrofit2.http.Path
 
 interface NaverPayService {
 
+    // 홈 화면
     @Headers("Content-Type: application/json", "X-Auth-id: 1")
     @GET("")
     suspend fun homeLogin(): BaseResponse<HomeResponse>
 
+    // 현장결제_장소 조회
     @Headers("Content-Type: application/json", "X-Auth-id: 1")
     @GET("/place")
     suspend fun getPlacePayment(): BaseResponse<PlaceResponse>
 
+    // 포인트_포인트 내역 조회
     @Headers("Content-Type: application/json", "X-Auth-id: 1")
     @GET("/point")
     suspend fun getPointInfo(): BaseResponse<PointResponse>
 
+    // 혜택_내역 조회
     @Headers("Content-Type: application/json", "X-Auth-id: 1")
     @GET("/benefit")
     suspend fun getBenefitInfo(): BaseResponse<BenefitResponse>
 
+    // 혜택_찜하기 등록
     @Headers("Content-Type: application/json", "X-Auth-id: 1")
     @POST("/benefit/like/{brandId}")
     suspend fun sendLike(
         @Path("brandId") brandId: String,
     ): BaseResponse<LikeResponse>
 
+    // 혜택_찜하기 취소
     @Headers("Content-Type: application/json", "X-Auth-id: 1")
     @DELETE("/benefit/like/{brandId}")
     suspend fun deleteLike(
         @Path("brandId") brandId: String,
     ): BaseResponse<LikeResponse>
 
+    // 혜택_포인트 추천 내역 조회
     @Headers("Content-Type: application/json")
     @GET("/benefit/recommend")
     suspend fun getRecommend(): BaseResponse<BenefitResponse>
