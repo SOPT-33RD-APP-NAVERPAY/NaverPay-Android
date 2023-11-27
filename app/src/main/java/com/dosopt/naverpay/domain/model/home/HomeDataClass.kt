8a15f2640a2d5f641b3cfd5e52cea2ff0b0d6c25
Junseo511 <prime54@naver.com>
2023-11-28 01:36:32 +0900

    [CHORE] : change data class format

--- app/src/main/java/com/dosopt/naverpay/domain/model/home/HomeDataClass.kt
+++ app/src/main/java/com/dosopt/naverpay/domain/model/home/HomeDataClass.kt
@@ -1,37 +1,22 @@
 package com.dosopt.naverpay.domain.model.home
 
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
-
-@Serializable
 data class CardInfo(
-    @SerialName("id") val id: Int,
-    @SerialName("img") val img: Int,
+    val id: Int,
+    val img: Int
 )
 
-@Serializable
 data class EventInfo(
-    @SerialName("id") val id: Int,
-    @SerialName("logo") val logo: Int,
-    @SerialName("detail") val detail: String,
-    @SerialName("percentage") val percentage: String,
-    @SerialName("background") val background: String,
+    val id: Int,
+    val logo: Int,
+    val detail: String,
+    val percentage: String,
+    val background: String
 )
 
-@Serializable
 data class BrandInfo(
-    @SerialName("id")
     val id: Long,
-
-    @SerialName("name")
     val name: String,
-
-    @SerialName("place")
     val place: String,
-
-    @SerialName("logo_img_url")
     val logoImgUrl: String,
-
-    @SerialName("discount_content")
-    val discountContent: String,
+    val discountContent: String
 )
