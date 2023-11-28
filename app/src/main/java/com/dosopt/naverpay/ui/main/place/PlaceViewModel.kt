a34dfe3dffaeb339ee44719a5bc21c28e46c7105
heominhoe <hhmmm1005@naver.com>
2023-11-28 14:34:29 +0900

    [FEAT] : delete mocklist and attach API to viewmodel

--- app/src/main/java/com/dosopt/naverpay/ui/main/place/PlaceViewModel.kt
+++ app/src/main/java/com/dosopt/naverpay/ui/main/place/PlaceViewModel.kt
@@ -1,119 +1,51 @@
 package com.dosopt.naverpay.ui.main.place
 
+import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.dosopt.naverpay.domain.model.place.BrandList
-import com.dosopt.naverpay.domain.model.place.NearbyplaceList
-import com.dosopt.naverpay.domain.model.place.OnsitepaymentList
-import com.dosopt.naverpay.domain.model.place.User
+import androidx.lifecycle.viewModelScope
+import com.dosopt.naverpay.network.ServicePool
+import com.dosopt.naverpay.network.dto.PlaceResponse
+import kotlinx.coroutines.launch
 
 class PlaceViewModel : ViewModel() {
-    val mockUser = MutableLiveData(
-        User(
-            "원희연"
-        )
-    )
-    val mockNearbyplaceList = MutableLiveData(
-        listOf(
-            NearbyplaceList(
-                id = 1,
-                name = "cu 편의점",
-                logo_img_url = "https://github.com/DO-SOPT-ANDROID/minhoe-heo/assets/145532333/08e7a512-ed0b-4197-b935-db471a684d2f",
-                distance = 24
-            ),
-            NearbyplaceList(
-                id = 2,
-                name = "cu 편의점",
-                logo_img_url = "https://github.com/DO-SOPT-ANDROID/minhoe-heo/assets/145532333/08e7a512-ed0b-4197-b935-db471a684d2f",
-                distance = 24
-            ),
-            NearbyplaceList(
-                id = 3,
-                name = "cu 편의점",
-                logo_img_url = "https://github.com/DO-SOPT-ANDROID/minhoe-heo/assets/145532333/08e7a512-ed0b-4197-b935-db471a684d2f",
-                distance = 24
-            ),
-            NearbyplaceList(
-                id = 4,
-                name = "cu 편의점",
-                logo_img_url = "https://github.com/DO-SOPT-ANDROID/minhoe-heo/assets/145532333/08e7a512-ed0b-4197-b935-db471a684d2f",
-                distance = 24
-            ),
-            NearbyplaceList(
-                id = 5,
-                name = "cu 편의점",
-                logo_img_url = "https://github.com/DO-SOPT-ANDROID/minhoe-heo/assets/145532333/08e7a512-ed0b-4197-b935-db471a684d2f",
-                distance = 24
-            ),
-            NearbyplaceList(
-                id = 6,
-                name = "cu 편의점",
-                logo_img_url = "https://github.com/DO-SOPT-ANDROID/minhoe-heo/assets/145532333/08e7a512-ed0b-4197-b935-db471a684d2f",
-                distance = 24
-            )
-        )
-    )
-    val mockBrandList = MutableLiveData(
-        listOf(
-            BrandList(
-                id = 1,
-                logo_img_url = "https://github.com/DO-SOPT-ANDROID/minhoe-heo/assets/145532333/08e7a512-ed0b-4197-b935-db471a684d2f"
-            ),
-            BrandList(
-                id = 2,
-                logo_img_url = "https://github.com/DO-SOPT-ANDROID/minhoe-heo/assets/145532333/08e7a512-ed0b-4197-b935-db471a684d2f"
-            ),
-            BrandList(
-                id = 3,
-                logo_img_url = "https://github.com/DO-SOPT-ANDROID/minhoe-heo/assets/145532333/08e7a512-ed0b-4197-b935-db471a684d2f"
-            ),
-            BrandList(
-                id = 4,
-                logo_img_url = "https://github.com/DO-SOPT-ANDROID/minhoe-heo/assets/145532333/08e7a512-ed0b-4197-b935-db471a684d2f"
-            ),
-            BrandList(
-                id = 5,
-                logo_img_url = "https://github.com/DO-SOPT-ANDROID/minhoe-heo/assets/145532333/08e7a512-ed0b-4197-b935-db471a684d2f"
-            ),
-            BrandList(
-                id = 6,
-                logo_img_url = "https://github.com/DO-SOPT-ANDROID/minhoe-heo/assets/145532333/08e7a512-ed0b-4197-b935-db471a684d2f"
-            )
-        )
-    )
-    val mockOnsitepaymentList = MutableLiveData(
-        listOf(
-            OnsitepaymentList(
-                id = 1,
-                logo_img_url = "https://github.com/DO-SOPT-ANDROID/minhoe-heo/assets/145532333/08e7a512-ed0b-4197-b935-db471a684d2f",
-                name = "도미노피자"
-            ),
-            OnsitepaymentList(
-                id = 2,
-                logo_img_url = "https://github.com/DO-SOPT-ANDROID/minhoe-heo/assets/145532333/08e7a512-ed0b-4197-b935-db471a684d2f",
-                name = "도미노피자"
-            ),
-            OnsitepaymentList(
-                id = 3,
-                logo_img_url = "https://github.com/DO-SOPT-ANDROID/minhoe-heo/assets/145532333/08e7a512-ed0b-4197-b935-db471a684d2f",
-                name = "도미노피자"
-            ),
-            OnsitepaymentList(
-                id = 4,
-                logo_img_url = "https://github.com/DO-SOPT-ANDROID/minhoe-heo/assets/145532333/08e7a512-ed0b-4197-b935-db471a684d2f",
-                name = "도미노피자"
-            ),
-            OnsitepaymentList(
-                id = 5,
-                logo_img_url = "https://github.com/DO-SOPT-ANDROID/minhoe-heo/assets/145532333/08e7a512-ed0b-4197-b935-db471a684d2f",
-                name = "도미노피자"
-            ),
-            OnsitepaymentList(
-                id = 6,
-                logo_img_url = "https://github.com/DO-SOPT-ANDROID/minhoe-heo/assets/145532333/08e7a512-ed0b-4197-b935-db471a684d2f",
-                name = "도미노피자"
-            )
-        )
-    )
+
+    private val _placeResult: MutableLiveData<PlaceResponse> = MutableLiveData()
+    val placeResult: LiveData<PlaceResponse> = _placeResult
+
+    private val _placeSuccess: MutableLiveData<Boolean> = MutableLiveData()
+    val placeSuccess: LiveData<Boolean> = _placeSuccess
+
+    private val _placeResponse = MutableLiveData<PlaceResponse>()
+    val placeResponse: LiveData<PlaceResponse> = _placeResponse
+
+    private val _nearbyplaceList = MutableLiveData<List<PlaceResponse.NearbyplaceListDto>>()
+    val nearbyplaceList: LiveData<List<PlaceResponse.NearbyplaceListDto>> = _nearbyplaceList
+
+    private val _brandList = MutableLiveData<List<PlaceResponse.BrandListDto>>()
+    val brandList: LiveData<List<PlaceResponse.BrandListDto>> = _brandList
+
+    private val _onsitepaymentList = MutableLiveData<List<PlaceResponse.OnsitepaymentListDto>>()
+    val onsitepaymentList: LiveData<List<PlaceResponse.OnsitepaymentListDto>> = _onsitepaymentList
+
+    private val _userName = MutableLiveData<String>()
+    val userName: LiveData<String> = _userName
+
+    fun getPlacePayment() {
+        viewModelScope.launch {
+            runCatching {
+                ServicePool.naverPayService.getPlacePayment()
+            }.onSuccess { response -> //BaseResponse<PlaceResponse>
+                _userName.value = response.data?.userName
+                _nearbyplaceList.value = response.data?.nearbyplaceList
+                _brandList.value = response.data?.brandList
+                _onsitepaymentList.value = response.data?.onsitepaymentList
+
+            }.onFailure {
+                Log.e("NetworkTest1", "error:$it")
+            }
+        }
+    }
 }
 
